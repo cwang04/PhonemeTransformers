3d1be2096891b016638562dd1a8eb0e63ba2f0d7
Zebulon Goriely <zepsilon32@gmail.com>
2025-01-29 10:12:02 +0000

    Ensure tokenizer changes for multilingual runs

--- scripts/launch_experiment_childes_multilingual.sh
+++ scripts/launch_experiment_childes_multilingual.sh
@@ -3,7 +3,7 @@ LANGUAGES=("English" "EnglishUK" "German" "Japanese" "Indonesian" "French" "Span
 for language in ${LANGUAGES[@]};
 do
     echo "Launching job for language: $language"
-    sbatch launch_slurm.wilkes3 experiment=childes_multilingual_03 experiment.name=$language dataset.subconfig=$language
+    sbatch launch_slurm.wilkes3 experiment=childes_multilingual_03 experiment.name=$language dataset.subconfig=$language tokenizer.name=phonemetransformers/CHILDES-$language-phoneme-tokenizer 
 done
 
 
